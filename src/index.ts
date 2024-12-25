a0096a924fd46cb1c445cafb32b45bc58f19ec68
HitomaruKonpaku <hunteratthenight@gmail.com>
2024-12-26 00:31:50 +0700

    Remove YouTube

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import 'dotenv/config'
 import { logger } from './logger'
 import { configManager } from './modules/ConfigManager'
 import { twitCastingCrawler } from './modules/TwitCastingCrawler'
-import { youTubeCrawler } from './modules/YouTubeCrawler'
 
 logger.info(Array(80).fill('=').join(''))
 
 async function main() {
   configManager.load()
   twitCastingCrawler.start()
-  youTubeCrawler.start()
+  // youTubeCrawler.start()
 }
 
 main()
